048d4a4c8ea521f59f3fe37c91821f70e0331113
Ansen123 <ansenfp@gmail.com>
2022-10-15 11:37:24 +0530

    api

--- src/app/contact-view/contact-view.component.ts
+++ src/app/contact-view/contact-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-contact-view',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) {
+    this.fetchData()
+   }
+   fetchData=()=>{
+    this.myapi.viewContact().subscribe(
+      (data)=>{
+        this.contact=data
+      }
+    )
+   }
 
+   contact:any={}
   ngOnInit(): void {
   }
 
